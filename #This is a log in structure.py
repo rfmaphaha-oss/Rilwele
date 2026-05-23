fafdd12d0f2093372da4569a4f302178910d3890
rfmaphaha-oss <rf.maphaha@gmail.com>
2026-05-24 01:01:50 +0300

    a log in profile structure

---
+++ #This is a log in structure.py
@@ -0,0 +1,20 @@
+while True: #This is a log in structure
+    print('Hello Programmer')
+    print('What is your name ?')
+    input=(str()) #enter your name here
+my_name = 'Rilwele'
+if my_name == 'Rilwele':
+        print( 'Hello ' + my_name + ' i hope your day is going well ')
+else:
+     print('Access not granted')
+     continue
+print('How old are you ?')
+input(int())
+my_age=="19"
+if my_age > '18':
+    print('Acess granted; you are turning'+ str(int(my_age)+1)+'years old next year.')
+else:
+    print('You are too young to log in on this page')
+
+print('Leave a comment about this website')
+commnent = input()
